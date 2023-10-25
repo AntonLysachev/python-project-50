f89dad3ba58e233704ab47b09b0b9318959cecb2
AntonLysachev <anton@lysachev.ru>
2023-10-25 15:37:48 +0500

    refactoring generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,23 +1,56 @@
-import argparse
 import json
 
 
-def generate_diff(file1, file2):
-    with open(file1, "r") as f1, open(file2, "r") as f2:
-        diff_file1 = dict(sorted(json.load(f1).items()))
-        diff_file2 = dict(sorted(json.load(f2).items()))
-        out = ''
-        diff_key = diff_file2.keys() - diff_file1.keys()
-        for key, value in diff_file1.items():
-            if value == diff_file2.get(key):
-                out = f'{out}   {key}: {value}\n'
-            elif diff_file2.get(key) == None:
-                out = f'{out} - {key}: {value}\n'
-            elif value != diff_file2.get(key):
-                out = f'{out} - {key}: {value}\n'
-                out = f'{out} + {key}: {diff_file2.get(key)}\n' 
-        for key in diff_key:
-            if diff_file1.get(key) == None:
-                out = f'{out} + {key}: {diff_file2.get(key)}\n'
-                
+def get_data(file):
+    with open(file, "r") as f:
+        data = json.load(f)
+    return data
+
+
+def parsing_files(data1, data2):
+    data = {}
+    only_one = data1.keys() - data2.keys()
+    only_tow = data2.keys() - data1.keys()
+    for key in only_one:
+        data.update({key: {'prefix': '-'}})
+        data[key].update({'name': key})
+        data[key].update({'data':  data1[key]})
+    for key in only_tow:
+        data.update({key: {'prefix': '+'}})
+        data[key].update({'name': key})
+        data[key].update({'data':  data2[key]})
+    for key, value in data1.items():
+        if data2.get(key):
+            if value == data2[key]:
+                data.update({key: {'prefix': ' '}})
+                data[key].update({'name': key})
+                data[key].update({'data':  data1[key]})
+            else:
+                data.update({f'{key}1': {'prefix': '-'}})
+                data[f'{key}1'].update({'name': key})
+                data[f'{key}1'].update({'data':  data1[key]})
+                data.update({f'{key}2': {'prefix': '+'}})
+                data[f'{key}2'].update({'name': key})
+                data[f'{key}2'].update({'data':  data2[key]})
+    
+    return dict(sorted(data.items()))
+
+
+def format(compare_data):
+    out= ''
+    for value in compare_data.values():
+        out = f'{out} {value["prefix"]} {value["name"]}: {value["data"]}\n'
     return f'{{\n{out}}}'
+
+
+    
+
+def generate_diff(file1, file2):
+    data_file1 = get_data(file1)
+    data_file2 = get_data(file2)
+    pars = parsing_files(data_file1, data_file2)
+    out = format(pars)           
+    return out
+
+
+# print(generate_diff('Test/file1.json', 'Test/file2.json'))
