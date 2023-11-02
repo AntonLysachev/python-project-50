f1e8248a471f3516c14b4f3c5fbed9a2789643f5
AntonLysachev <anton@lysachev.ru>
2023-11-02 10:29:15 +0500

    update after lint

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -1,5 +1,4 @@
 import json
-import copy
 
 
 def get_data(file):
@@ -9,29 +8,15 @@ def get_data(file):
 
 
 def parsing(file1, file2):
-    date_file1 = get_data(file1)
-    date_file2 = get_data(file2)
-    merg = copy.deepcopy(date_file1)
-    merg.update(date_file2)
-
-    def pars(merg_date, date1, date2):
-        parsing = {}
-        for key, value in merg_date.items():
-            if key in date1 and key in date2:
-                if isinstance(value, dict):
-                    parsing.update({key: pars(value, date1[key], date2[key])})
-                    continue
-                if date1[key] == date2[key]:
-                    parsing.update({key: f'  {key}: {date1[key]}'})
-                    continue
-                parsing.update({f'{key}1': f'- {key}: {date1[key]}',
-                                f'{key}2': f'+ {key}: {date2[key]}'})
-                continue
-            else:
-                if key in date1:
-                    parsing.update({key: f'- {key}: {date1[key]}'})
-                    continue
-                parsing.update({key: f'+ {key}: {date2[key]}'})
-        return parsing
-    out = pars(merg, date_file1, date_file2)
-    return dict(sorted(out.items()))
+    date1 = get_data(file1)
+    date2 = get_data(file2)
+    parsing = {}
+    for key in date1.keys():
+        if date1[key] == date2.get(key):
+            parsing.update({key: f'  {key}: {date1[key]}'})
+        else:
+            parsing.update({key: f'- {key}: {date1[key]}'})
+    for key in date2.keys():
+        if date2[key] != date1.get(key):
+            parsing.update({f'{key}2': f'+ {key}: {date2[key]}'})
+    return dict(sorted(parsing.items()))
