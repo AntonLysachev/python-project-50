53e661519bb2716979e861a00b242d5c1b4d1417
AntonLysachev <anton@lysachev.ru>
2023-11-03 16:09:03 +0500

    update parsing

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -1,11 +1,11 @@
 def parsing(date1, date2):
     analyzed = {}
-    for key in date1.keys():
-        if date1[key] == date2.get(key):
-            analyzed.update({key: f'  {key}: {date1[key]}'})
+    for key, value in date1.items():
+        if value == date2.get(key):
+            analyzed.update({key: f'  {key}: {value}'})
         else:
-            analyzed.update({key: f'- {key}: {date1[key]}'})
-    for key in date2.keys():
-        if date2[key] != date1.get(key):
-            analyzed.update({f'{key}2': f'+ {key}: {date2[key]}'})
+            analyzed.update({key: f'- {key}: {value}'})
+    for key, value in date2.items():
+        if value != date1.get(key):
+            analyzed.update({f'{key}2': f'+ {key}: {value}'})
     return dict(sorted(analyzed .items()))
