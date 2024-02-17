2866e4d3fd20d3ccebe43928f2833571ad26515d
AntonLysachev <anton@lysachev.ru>
2024-02-17 10:08:34 +0500

    add README

--- gendiff/generate_diff_tree.py
+++ gendiff/generate_diff_tree.py
@@ -10,24 +10,22 @@ def build_diff_tree(data1, data2):
     for key in all_keys:
         value1 = data1.get(key)
         value2 = data2.get(key)
-        diff.update({key: {}})
+        # diff.update({key: {}})
+        diff[key] = {}
         if isinstance(value1, dict) and isinstance(value2, dict):
             diff[key].update({'type': 'nested', 'children':
-                              build_diff_tree(data1[key], data2[key])})
-            continue
-        if key in removed_keys:
-            diff[key].update({'type': 'removed', 'value': value1})
-            continue
-        if key in added_keys:
+                              build_diff_tree(value1, value2)})
+        elif key in removed_keys:
+            diff[key].update({'type': 'removed', 'value': value1})  
+        elif key in added_keys:
             diff[key].update({'type': 'added', 'value': value2})
-            continue
-        if value1 == value2:
+        elif value1 == value2:
             diff[key].update({'type': 'not updated',
                               'value': value1})
-            continue
-        diff[key].update({'type': 'updated',
-                          'old_value': value1,
-                          'new_value': value2})
+        else:
+            diff[key].update({'type': 'updated',
+                            'old_value': value1,
+                            'new_value': value2})
     return dict(sorted(diff.items()))
 
 
